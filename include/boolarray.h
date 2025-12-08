36d2e36159a02447358d6e1080bc2177b41c7d84
Brulans4 <nicolas.heymans@hotmail.com>
2025-12-08 18:14:27 +0100

    doc (boolarray): addAt, get and set functions

--- include/boolarray.h
+++ include/boolarray.h
@@ -58,15 +58,53 @@ bool boolarray_isEmpty(const BoolArray *bArray);
  */
 bool boolarray_add(BoolArray *bArray, bool value);
 
+/**
+ * Inserts a boolean value at the specified index in the BoolArray.
+ * All elements from the insertion index to the end of the array are shifted
+ * one position to the right. The array automatically resizes if needed.
+ *
+ * @param bArray Pointer to the BoolArray.
+ * @param value The boolean value to insert.
+ * @param index The position at which the value should be inserted.
+ *
+ * @return true if the value was successfully inserted,
+ *         false if bArray is NULL, index is out of bounds,
+ *         memory allocation for shifting fails, or resize fails.
+ */
 bool boolarray_addAt(BoolArray *bArray, bool value, size_t index);
 
 bool *boolarray_remove(BoolArray *bArray);
 
 bool *boolarray_removeAt(BoolArray *bArray, size_t index);
 
+/**
+ * Retrieves the boolean value stored at the specified index.
+ *
+ * @param bArray Pointer to the BoolArray.
+ * @param index The index of the element to retrieve.
+ *
+ * @return A pointer to a dynamically allocated boolean
+ *         containing the value at the specified index,
+ *         or NULL if bArray is NULL, index is out of range,
+ *         or memory allocation fails.
+ *
+ * @warning The returned pointer MUST be freed by the caller.
+ */
 bool *boolarray_get(BoolArray *bArray, size_t index);
 
-bool boolarray_set(BoolArray *bArray, size_t index);
+/**
+ * Sets the boolean value at the specified index in the BoolArray.
+ *
+ * @param bArray Pointer to the BoolArray. Must not be NULL.
+ * @param index The index to modify (0 <= index < size).
+ * @param value The boolean value to store.
+ *
+ * @return true if the value was successfully stored,
+ *         false if bArray is NULL or index is out of bounds.
+ *
+ * @note Updates only one bit inside the correct byte.
+ */
+bool boolarray_set(BoolArray *bArray, size_t index, bool value);
 
 void boolarray_flip(BoolArray *bArray, size_t index);
 
