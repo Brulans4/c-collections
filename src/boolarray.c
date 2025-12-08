908358b7b7c5a6b9fc451eb20a0a81d85ccc6700
Brulans4 <nicolas.heymans@hotmail.com>
2025-12-08 14:14:03 +0100

    feat (boolarray): impl add function

--- src/boolarray.c
+++ src/boolarray.c
@@ -7,10 +7,12 @@ typedef struct BoolArray
     uint8_t *data;
 } BoolArray;
 
+static bool boolarray_resizeList(BoolArray *bArray);
+
 BoolArray *boolarray_create()
 {
-    BoolArray* bArray = malloc(sizeof(BoolArray));
-    uint8_t* pInt8 = malloc(sizeof(uint8_t));
+    BoolArray *bArray = malloc(sizeof(BoolArray));
+    uint8_t *pInt8 = malloc(sizeof(uint8_t));
 
     if (bArray == NULL || pInt8 == NULL)
     {
@@ -19,7 +21,7 @@ BoolArray *boolarray_create()
     bArray->capacity = sizeof(uint8_t);
     bArray->size = 0;
     bArray->data = pInt8;
-    
+
     return bArray;
 }
 
@@ -49,10 +51,54 @@ bool boolarray_isEmpty(const BoolArray *bArray)
 
 bool boolarray_add(BoolArray *bArray, bool value)
 {
+    if (bArray == NULL)
+    {
+        return false;
+    }
+    if (value == true)
+    {
+        size_t index = (bArray->size >> 3);
+        uint8_t bitIndex = (bArray->size - index);
+
+        uint8_t bitmask = 0x01 << bitIndex;
+
+        uint8_t* dataArray = bArray->data;
+        dataArray[index] = dataArray[index] | bitmask;
+    }
+    bArray->size++;
+
+    if (bArray->size == bArray->capacity)
+    {
+        return boolarray_resizeList(bArray);
+    }
+    return true;
 }
 
 bool boolarray_addAt(BoolArray *bArray, bool value, size_t index)
 {
+}
+
+static bool boolarray_resizeList(BoolArray *bArray)
+{
+    size_t newCapacity = (bArray->capacity >> 3) * 2;
+    uint8_t *newData = malloc(newCapacity * sizeof(uint8_t));
+
+    if (!newData)
+    {
+        return false;
+    }
+
+    uint8_t *oldData = (uint8_t *)bArray->data;
+
+    memcpy(newData,
+           oldData,
+           (bArray->capacity >> 3) * sizeof(uint8_t));
+    free(oldData);
+
+    bArray->data = newData;
+    bArray->capacity = newCapacity;
+
+    return true;
 }
 
 bool *boolarray_remove(BoolArray *bArray)
